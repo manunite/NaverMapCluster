26d3a6b248f57723114a1301f37830570ecea10e
허규진 <heogj123@tmon.co.kr>
2022-06-28 20:54:24 +0900

    (*) Framework 추가.

---
+++ NaverMapClusterFramework.xcframework/ios-arm64/NaverMapClusterFramework.framework/Headers/NaverMapClusterFramework.h
@@ -0,0 +1,18 @@
+//
+//  NaverMapClusterFramework.h
+//  NaverMapClusterFramework
+//
+//  Created by heogj123 on 2022/06/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NaverMapClusterFramework.
+FOUNDATION_EXPORT double NaverMapClusterFrameworkVersionNumber;
+
+//! Project version string for NaverMapClusterFramework.
+FOUNDATION_EXPORT const unsigned char NaverMapClusterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NaverMapClusterFramework/PublicHeader.h>
+
+
